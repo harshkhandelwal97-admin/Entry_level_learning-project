319a14cefc706d8e2d4348505b35792805f7cb43
harshkhandelwal97-admin <harsh.khandelwal@recykal.com>
2025-05-23 15:10:36 +0530

    add

--- src/main/java/com/example/ECommerce/ECommerceApplication.java
+++ src/main/java/com/example/ECommerce/ECommerceApplication.java
@@ -11,8 +11,8 @@ import java.sql.SQLException;
 
 @SpringBootApplication
 public class ECommerceApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
