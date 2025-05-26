9d161b4b18e76d2e544a2a2745a21ef995610aae
harshit <harshit.shambharkar@recykal.com>
2025-05-26 15:54:18 +0530

    adding product implementation

--- src/main/java/com/example/ECommerce/controllers/ProductController.java
+++ src/main/java/com/example/ECommerce/controllers/ProductController.java
@@ -1,8 +1,10 @@
 package com.example.ECommerce.controllers;
 
+import com.example.ECommerce.dto.ApiResponse;
 import com.example.ECommerce.entities.Product;
 import com.example.ECommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,34 +23,57 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        Product newProduct = productService.createProduct(product);
-        return ResponseEntity.created(URI.create("/api/products/" + newProduct.getProductId()))
-                .body(newProduct);
+    @PostMapping("/new")
+    public ResponseEntity<ApiResponse<Product>> createProduct(@RequestBody Product product) {
+        Product savedProduct = productService.createProduct(product);
+        ApiResponse<Product> response = new ApiResponse<>(
+                true,
+                "New product created successfully",
+                savedProduct
+        );
+        return ResponseEntity.created(URI.create("/api/products/" + savedProduct.getProductId()))
+                .body(response);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<Product>> getProductById(@PathVariable Long id) {
         Optional<Product> productOpt = productService.getProductById(id);
-        return productOpt
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+        return productOpt.isPresent() ? ResponseEntity.ok().body(new ApiResponse<>(
+                true,
+                "Product fetched successfully",
+                productOpt.orElse(null))
+        ) : ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Product not available", null));
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<Product>> updateProductById(@PathVariable Long id, @RequestBody Product product) {
+        Optional<Product> productOpt = productService.getProductById(id);
+        if (productOpt.isPresent()) {productService.updateProduct(product);}
+        return productOpt.map(value ->
+            ResponseEntity.ok().body(new ApiResponse<>(
+                true,
+                "Product updated successfully",
+                value
+        ))).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Product not available", null)));
     }
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
+    public ResponseEntity<ApiResponse<List<Product>>> getAllProducts() {
         List<Product> products =  productService.getAllProduct();
-        return ResponseEntity.ok(products);
+        String message = products.isEmpty() ? "No products available" : "Products listed below";
+        ApiResponse<List<Product>> response = new ApiResponse<>(true, message, products);
+        return ResponseEntity
+                .ok(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProductById(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<Product>> deleteProductById(@PathVariable Long id) {
         boolean isDeleted = productService.deleteProduct(id);
         if(isDeleted) {
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok().body(new ApiResponse<>(true, "Product deleted successfully", null));
         }
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, "Cannot find the product", null));
     }
 
 }
