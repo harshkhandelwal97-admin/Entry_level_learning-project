ebfb59fcbd2bf0cfb7a814f9bb4ecb7653272d0f
harshit <harshit.shambharkar@recykal.com>
2025-05-27 16:18:18 +0530

    adding model mapper

--- src/main/java/com/example/ECommerce/ECommerceApplication.java
+++ src/main/java/com/example/ECommerce/ECommerceApplication.java
@@ -1,19 +1,20 @@
 package com.example.ECommerce;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ECommerceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
