4b78137cb1b597f60136198d4cdeec574fbf9143
harshit <harshit.shambharkar@recykal.com>
2025-05-27 16:51:48 +0530

    fixing product entity

--- src/main/java/com/example/ECommerce/entities/Product.java
+++ src/main/java/com/example/ECommerce/entities/Product.java
@@ -1,5 +1,6 @@
 package com.example.ECommerce.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,19 +12,24 @@ import lombok.Setter;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
+    @JsonProperty(value = "product_id")
     private Long productId;
 
     @Column(name = "product_name")
+    @JsonProperty(value = "product_name")
     private String productName;
 
     @Column(name = "product_price")
+    @JsonProperty(value = "product_price")
     private Double productPrice;
 
     @Column(name = "image_url")
+    @JsonProperty(value = "image_url")
     private String imageUrl;
 
     @Column(name = "product_description")
+    @JsonProperty(value = "product_description")
     private String productDescription;
 }
