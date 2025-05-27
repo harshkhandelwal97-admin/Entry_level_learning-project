e0760b9b60f5eb0c211eca444d11f9cd0f3d5b0f
harshit <harshit.shambharkar@recykal.com>
2025-05-27 15:00:06 +0530

    adding product dto

--- src/main/java/com/example/ECommerce/dto/CartItemsDto.java
+++ src/main/java/com/example/ECommerce/dto/CartItemsDto.java
@@ -1,15 +1,16 @@
 package com.example.ECommerce.dto;
 
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Data
-//public class CartItemsDto {
-//    private Long cartItemId;
-//    private CartDto cart;
-//    private ProductDto product;
-//    private int quantity;
-//}
+import com.example.ECommerce.entities.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CartItemsDto {
+    private Long cartItemId;
+    private CartDto cart;
+    private ProductDTO product;
+    private int quantity;
+}
---
+++ src/main/java/com/example/ECommerce/dto/ProductDTO.java
@@ -0,0 +1,17 @@
+package com.example.ECommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO {
+
+    private Long productId;
+    private String productName;
+    private String productDescription;
+    private double productPrice;
+    private String imageUrl;
+}
