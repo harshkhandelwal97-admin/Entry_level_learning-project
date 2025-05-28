de6deec2cda22cf2708380cc97a6a1e2937a6d89
nandakishore654 <nandakishore.mummadi@recykal.com>
2025-05-28 12:31:22 +0530

    Added show the cart API

--- src/main/java/com/example/ECommerce/controllers/CartController.java
+++ src/main/java/com/example/ECommerce/controllers/CartController.java
@@ -1,5 +1,6 @@
 package com.example.ECommerce.controllers;
 
+import com.example.ECommerce.dto.CartDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +14,22 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/user/{userid}/cart/{cartid}")
+@RequestMapping("/api")
 //add the following mappings in the controller
 //1) Add to the Cart
 //2) Delete from the cart
 //3) Show the whole cart
 public class CartController {
-    //@PostMapping("/")
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @GetMapping("/user/{userid}/cart/{cartid}")
+    public ResponseEntity<ApiResponse<List<CartItemsDto>>> getCart(@PathVariable Long cartid) {
+        List<CartItemsDto> cartDtoList = cartItemService.showAlltheProducts(cartid);
+        String message = cartDtoList.isEmpty() ? "No cart found" : "Below are the products in the cart";
+        ApiResponse<List<CartItemsDto>> apiResponse = new ApiResponse<>(true, message, cartDtoList);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
 }
