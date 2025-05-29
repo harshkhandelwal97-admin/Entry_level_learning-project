6176fc71761f3101c6770a911538cc5c935a7446
harshkhandelwal97-admin <harsh.khandelwal@recykal.com>
2025-05-29 12:55:09 +0530

    added a method for getting total price

--- src/main/java/com/example/ECommerce/services/PaymentHistoryService.java
+++ src/main/java/com/example/ECommerce/services/PaymentHistoryService.java
@@ -1,36 +1,38 @@
 package com.example.ECommerce.services;
-
 import com.example.ECommerce.dto.PaymentDetailsDTO;
-import com.example.ECommerce.dto.PaymentRequest;
-import com.example.ECommerce.dto.ProductDTO;
 import com.example.ECommerce.entities.Cart;
 import com.example.ECommerce.entities.CartItem;
 import com.example.ECommerce.entities.Payments;
 import com.example.ECommerce.entities.User;
+import com.example.ECommerce.repositories.CartItemRepo;
 import com.example.ECommerce.repositories.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class PaymentHistoryService {
 
     @Autowired
     private PaymentRepository paymentRepository;
+    @Autowired
+    private CartItemRepo cartItemRepo;
 
     public PaymentDetailsDTO getPaymentDetails(Long paymentId) {
-        Payments payment = paymentRepository.findById(paymentId)
-                .orElseThrow(() -> new RuntimeException("Cart not found"));
+        Payments payment = paymentRepository.findById(paymentId).orElseThrow(() -> new RuntimeException("Cart not found"));
         Long payId = payment.getPaymentId();
         User user = payment.getCustomer_id();
-        String username = payment.getCustomer_id().getName();
+        String username = user.getName();
         String paymentMethod = payment.getPaymentMethod();
         Cart cart = payment.getCart();
-        Double totalPrice = payment.getCart().getPrice();
-        return new PaymentDetailsDTO(payId, username, totalPrice, paymentMethod);
-
-
+        List<CartItem> cartItems = cartItemRepo.findByCartId(cart.getCartId()); // Assuming getCartItems() returns List<CartItem>
+        Double totalPrice = 0.0;
+        for (CartItem item : cartItems) {
+            Double itemTotal = item.getProduct().getProductPrice() * item.getQuantity();
+            totalPrice += itemTotal;
+        }
+        Double totalPrices = totalPrice;
+        return new PaymentDetailsDTO(payId, username, totalPrices, paymentMethod);
     }
 }
