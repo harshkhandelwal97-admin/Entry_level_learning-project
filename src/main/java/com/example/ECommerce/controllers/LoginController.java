48d635bada5ef76580ac10c16dafaa60dd689921
harshkhandelwal97-admin <harsh.khandelwal@recykal.com>
2025-05-30 16:23:57 +0530

    added some files for oauth

---
+++ src/main/java/com/example/ECommerce/controllers/LoginController.java
@@ -0,0 +1,14 @@
+//package com.example.ECommerce.controllers;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class LoginController {
+//    @GetMapping("/grantcode")
+//    public String grantCode(@RequestParam("code") String code, @RequestParam("scope") String scope, @RequestParam("authuser") String authUser, @RequestParam("prompt") String prompt) {
+//         return processGrantCode(code);
+//    }
+//}
